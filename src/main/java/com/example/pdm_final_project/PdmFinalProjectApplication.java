82d07de2826c5bc8ffad10704dcba813be6d2bf2
yahuy1525 <huytrian2005@gmail.com>
2024-11-26 18:40:22 +0700

    Init Commit

---
+++ src/main/java/com/example/pdm_final_project/PdmFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdm_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdmFinalProjectApplication.class, args);
+    }
+
+}
