931db2012f8ec68bfc9aa9d6d3378db89481746e
yahuy1525 <huytrian2005@gmail.com>
2024-11-27 17:20:01 +0700

    2nd commit

--- src/main/java/com/example/pdm_final_project/Repository/TaskRepository.java
+++ src/main/java/com/example/pdm_final_project/Repository/TaskRepository.java
@@ -4,5 +4,4 @@ import com.example.pdm_final_project.Entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
-
 }
